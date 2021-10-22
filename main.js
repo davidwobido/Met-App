c08dec9004331a944fb88aef53b9a31f13ae60f3
davidwobido <davidwobido@googlemail.com>
2021-10-22 11:35:18 +0200

    :sparkles: Get a response from met museum

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ async function renderApp() {
 
   const headerElement = createElement("header", { textContent: "header" });
   const mainElement = createElement("main", { textContent: "main" });
+
+  async function fetchImages() {
+    const response = await fetch(
+      "https://collectionapi.metmuseum.org/public/collection/v1/objects"
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+
   const footerElement = createElement("footer", { textContent: "footer" });
 
-  appElement.append(headerElement, mainElement, footerElement);
+  appElement.append(headerElement, fetchImages(), mainElement, footerElement);
 }
 renderApp();
